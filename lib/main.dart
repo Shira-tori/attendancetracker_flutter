af2295de1a86506932bb0b3be49e9d7ab5473bf1
Shira-tori <95753341+Shira-tori@users.noreply.github.com>
2023-01-28 15:28:12 +0800

    ewan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
+import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
-import 'package:mysql1/mysql1.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:attendify_/sql.dart';
+import 'package:open_file/open_file.dart';
 
 void main() async {
   runApp(const AttendanceApp());
@@ -16,7 +19,8 @@ class AttendanceApp extends StatelessWidget {
     return MaterialApp(
       home: const LoginPage(),
       routes: {
-        '/homescreen': (context) => const HomeScreen(),
+        '/teachers_homescreen': (context) => const HomeScreen(),
+        '/students_homescreen': (context) => const StudentsHomeScreen(),
       },
       debugShowCheckedModeBanner: false,
     );
@@ -151,9 +155,19 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                       Database db = Database();
                       var conn = await db.getConnection();
                       var result = await conn.query(
-                          'SELECT username, password FROM users_tbl WHERE username = "${usernameController.text}" AND password = "${passwordController.text}"');
+                          'SELECT fullname, username, password, role_id FROM users_tbl WHERE username = "${usernameController.text}" AND password = "${passwordController.text}"');
                       if (result.isNotEmpty) {
-                        Navigator.of(context).pushNamed("/homescreen");
+                        for (var row in result) {
+                          if (row[3] == 1) {
+                            // ignore: use_build_context_synchronously
+                            Navigator.pushNamed(context, "/teachers_homescreen",
+                                arguments: row[0]);
+                          } else {
+                            // ignore: use_build_context_synchronously
+                            Navigator.pushNamed(
+                                context, "/students_homescreen");
+                          }
+                        }
                       } else {
                         setState(() {
                           failed = true;
@@ -221,8 +235,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static List<Widget> widgets = [];
+  static List<String?> namesOfStudents = [];
+  @override
+  void dispose() {
+    widgets = [];
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as String;
+    List<String> name = args.split(" ");
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -233,10 +257,32 @@ class _HomeScreenState extends State<HomeScreen> {
             fontFamily: "Muli-Bold",
             fontSize: 20,
           ),
-          bottom: const TabBar(
+          bottom: TabBar(
             tabs: <Widget>[
-              Tab(text: "Home"),
-              Tab(text: "Camera"),
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.home),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text('Home')
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.camera),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text('Camera')
+                  ],
+                ),
+              ),
             ],
           ),
           leading: Builder(builder: (BuildContext context) {
@@ -250,16 +296,72 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: TabBarView(
           children: <Widget>[
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/bg2.jpg"),
-                    fit: BoxFit.cover,
-                    colorFilter:
-                        ColorFilter.mode(Color(0xdd1C4274), BlendMode.darken)),
+            Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () async {
+                  Permission.accessMediaLocation.request();
+                  List<Directory>? saveDir =
+                      await getExternalStorageDirectories(
+                          type: StorageDirectory.documents);
+                  String directory = saveDir![0].path;
+                  print(directory);
+                  var now = DateTime.now();
+                  var attendanceExcel = Excel.createExcel();
+                  Sheet attendanceSheet =
+                      attendanceExcel['${now.year}-${now.month}-${now.day}'];
+                  attendanceSheet.cell(CellIndex.indexByString('A1')).value =
+                      'STUDENT';
+                  attendanceSheet.cell(CellIndex.indexByString('B1')).value =
+                      'TIME';
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: const Text("Export"),
+                      content: const Text("Are you sure?"),
+                      actions: [
+                        TextButton(
+                            onPressed: () async {
+                              var counter = 2;
+                              for (var names
+                                  in _HomeScreenState.namesOfStudents) {
+                                attendanceSheet
+                                    .cell(CellIndex.indexByString('A$counter'))
+                                    .value = names?.split('::')[0];
+                                attendanceSheet
+                                    .cell(CellIndex.indexByString('B$counter'))
+                                    .value = names?.split('::')[1];
+                                counter++;
+                              }
+                              List<int>? fileBytes = attendanceExcel.save();
+
+                              File(
+                                  "$directory/${now.year}-${now.month}-${now.day}.xlsx")
+                                ..createSync(recursive: true)
+                                ..writeAsBytes(fileBytes!);
+                              Navigator.pop(context);
+                              OpenFile.open(
+                                  "$directory/${now.year}-${now.month}-${now.day}.xlsx");
+                            },
+                            child: const Text("Yes")),
+                        TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text("No")),
+                      ],
+                    ),
+                  );
+                },
+                child: const Icon(Icons.addchart),
               ),
-              child: const Center(
-                child: Text("Hi"),
+              body: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/bg2.jpg"),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Color(0xdd1C4274), BlendMode.darken)),
+                ),
+                child:
+                    ListView(addAutomaticKeepAlives: false, children: widgets),
               ),
             ),
             Container(
@@ -283,27 +385,79 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text(
-                    "Home",
-                    style: TextStyle(
-                      fontFamily: "Muli-Bold",
-                      color: Colors.white,
+                SizedBox(
+                  height: 200,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/drawerbg.jpg"),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Color(0xf11C4274), BlendMode.darken),
+                      ),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 50,
+                                child: Text("${name[0][0]}${name[1][0]}",
+                                    style: const TextStyle(fontSize: 30)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 10, bottom: 20, right: 10),
+                          child: Row(
+                            children: [
+                              Text(
+                                args,
+                                style: const TextStyle(
+                                  fontFamily: "Muli-Bold",
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  onTap: () => null,
                 ),
-                ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text("Logout"),
-                  onTap: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                        "/", (Route<dynamic> route) => false);
-                  },
+                Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.home),
+                      title: const Text(
+                        "Home",
+                        style: TextStyle(
+                          fontFamily: "Muli-Bold",
+                          color: Colors.white,
+                        ),
+                      ),
+                      onTap: () => null,
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.logout),
+                      title: const Text("Logout"),
+                      onTap: () {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            "/", (Route<dynamic> route) => false);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -389,12 +543,65 @@ class _QrCodeScannerState extends State<QrCodeScanner> {
           result = scanData;
           String? resultText = result!.code;
           prevText = resultText;
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text("$resultText"),
-              duration: const Duration(seconds: 2),
-            ),
-          );
+          bool repeated = false;
+
+          for (var name in _HomeScreenState.namesOfStudents) {
+            if (resultText?.split('::')[0] == name) {
+              repeated = true;
+              break;
+            } else {
+              repeated = false;
+            }
+          }
+
+          if (repeated == false) {
+            try {
+              if (resultText?.split(':')[1] == 'SECRET_KEY') {
+                var hour = DateTime.now().hour;
+                var minute = DateTime.now().minute < 10
+                    ? '0${DateTime.now().minute}'
+                    : DateTime.now().minute;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content:
+                        Text("${resultText?.split(":")[0]}::$hour:$minute"),
+                    duration: const Duration(seconds: 2),
+                  ),
+                );
+                _HomeScreenState.widgets.add(
+                  Column(
+                    children: [
+                      ListTile(
+                        tileColor: Colors.yellow,
+                        title: Text(
+                          '${resultText?.split(':')[0]}::$hour:$minute',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        onTap: () => null,
+                      ),
+                    ],
+                  ),
+                );
+                _HomeScreenState.namesOfStudents
+                    .add("${resultText?.split(':')[0]}::$hour:$minute");
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("INVALID QR CODE"),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              }
+            } catch (e) {
+              print("An error has occured.");
+            }
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text("This QR Code has already been scanned."),
+              ),
+            );
+          }
         }
       });
     });
@@ -404,5 +611,23 @@ class _QrCodeScannerState extends State<QrCodeScanner> {
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+}
+
+class StudentsHomeScreen extends StatefulWidget {
+  const StudentsHomeScreen({super.key});
+
+  @override
+  State<StudentsHomeScreen> createState() => _StudentsHomeScreenState();
+}
+
+class _StudentsHomeScreenState extends State<StudentsHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+    );
   }
 }
